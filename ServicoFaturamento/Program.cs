249604c866c202f140c1acf45dfe02a45463871a
Darion Forte <85178125+fdarionf@users.noreply.github.com>
2025-11-07 19:30:21 -0300

    Configura CORS em ambos os microsserviços para permitir o app Angular.

--- ServicoFaturamento/Program.cs
+++ ServicoFaturamento/Program.cs
@@ -1,17 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 
-builder.Services.AddHttpClient();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowAngularApp",
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:4200")
+                                .AllowAnyHeader()
+                                .AllowAnyMethod();
+                      });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
 app.UseHttpsRedirection();
-
+app.UseCors("AllowAngularApp");
 app.UseAuthorization();
 
 app.MapControllers();
